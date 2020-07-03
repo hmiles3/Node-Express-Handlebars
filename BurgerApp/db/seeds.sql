db686489feb6b69e35da921bb78ccd8a98bccefa
Hank Miles <Hankshadow2000@yahoo.com>
2020-07-02 22:27:13 -0400

    first try

---
+++ BurgerApp/db/seeds.sql
@@ -0,0 +1,5 @@
+USE burger_db;
+
+INSERT INTO burgers (burger_name, devoured) VALUES ('All-American', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Fat Elvis', true);
